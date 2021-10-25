01a731e9a9f97f4184a842b2ebf451d3b9236407
Mayuridalavai <mayuri.gj@gmail.com>
2021-10-25 08:20:07 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -17,6 +17,16 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
+// describe method that lists the name of the function
+// test method describes what function does
+// an expect method calls the function to test arguments
+
+  describe("capitalizeName",() => {
+  it('an array of objects and returns an array with a sentence about each person with their name capitalized.',() =>{
+  expect(capitalizeName(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+  })
+
 var people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
@@ -28,11 +38,41 @@ var people = [
 
 // b) Create the function that makes the test pass.
 
+//define a function named capitalizeName
+//map is used to iterate array 
+//declate variable called strings 
+//split the strimg 
+//use map to iterate the string and make the first character to uppercase
+//join the string 
+//add strings to `is occupation value`
+
+ const capitalizeName = (array)=>{
+ return array.map(value => {
+  let Strings = (value.name).toLowerCase().split(' ').map(value=> value.charAt(0).toUpperCase() + value.substring(1)).join(' ') 
+  return Strings + " " + `is ${value.occupation}.`
+})
+ }
+ 
+
+
+
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
+
+// describe method that lists the name of the function
+// test method describes what function does
+// an expect method calls the function to test arguments
+
+describe("remainderNum",() => {
+  it('a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.',() =>{
+  expect(remainderNum(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+  expect(remainderNum(hodgepodge2)).toEqual([ 2, 1, -1 ])
+
+  })
+  })
 
 var hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
@@ -43,11 +83,41 @@ var hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 
 // b) Create the function that makes the test pass.
 
+//define a function named reamainderNum
+//declare a newarray 
+//declare numarray and filter the array typeof number
+//for loop to iterate the array 
+//declare  num as numarray module 3
+//.push(num) into newarray
+
+ const remainderNum = (array)=>{
+  let newarray = []
+  let numarray=array.filter(value=>typeof value ==="number")
+  for(let i=0;i<numarray.length;i++){
+    let num = numarray[i]%3
+    newarray.push(num)
+ }return newarray
+   }
+console.log(remainderNum(hodgepodge1))
+console.log(remainderNum(hodgepodge2))
+
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
+
+// describe method that lists the name of the function
+// test method describes what function does
+// an expect method calls the function to test arguments
+
+describe("sumCube",() => {
+  it('an array of numbers and returns the sum of all the numbers cubed.',() =>{
+  expect(sumCube(cubeAndSum1)).toEqual(99)
+  expect(sumCube(cubeAndSum2)).toEqual(1125)
+
+  })
+  })
 
 var cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
@@ -57,3 +127,14 @@ var cubeAndSum2 = [0, 5, 10]
 
 
 // b) Create the function that makes the test pass.
+
+//declare a function named sumCube
+//map is used to iterate an array
+//value**3 is used to get cube number
+//.reduce is used for addtion of numbers in an array
+const sumCube = (array)=> {
+ return newarrray = array.map(value=>value**3).reduce((accumulator,current)=>accumulator+current,0)
+  
+}
+
+
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1)inheire  child class Blogpostscontroller from parent component ApplicationController 
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2)here we will access  all information about BlogPost from database save all the content from the database as an instance variable.
     @posts = BlogPost.all
   end
 
   def show
-    # ---3)
+    # ---3)Here we will get one item of particular data using id of item BlogPost in the database.This id will come from params
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4)It is a form we are able to add information to our web application that is then stored in the database.
   def new
     @post = Post.new
   end
 
   def create
-    # ---5)
+    # ---5)it creating or adding new items or content to the database
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -33,14 +33,14 @@ class BlogPostsController < ApplicationController
     end
   end
 
-  # ---6)
+  # ---6)It is a form we are able to update information to our web application. find the item and store in the database.
   def edit
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7)active record update to update only specific colomns using params
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8)after delete stay on that blog_posts show page 
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9)Anything below the word private cannot be accessed ouside the scope of this calss
   private
   def blog_post_params
-    # ---10)
+    # ---10)strong params method. It allows restriction to access particular columns that is  tiltes and content only from the ouside
     params.require(:blog_post).permit(:title, :content)
   end
 end
